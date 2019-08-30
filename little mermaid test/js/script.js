21cfbe9c35e23b9153222d8c2e0909e40ade7fa1
beccatess <beccatess@gmail.com>
2019-08-30 14:15:36 -0400

    Update script.js

--- little mermaid test/js/script.js
+++ little mermaid test/js/script.js
@@ -1,12 +1,7 @@
 document.getElementById("boat-buttons").style.visibility = "hidden";
 
-var myNameIs = (function() {
-  var mermaidName = document.getElementById('mermaid-name').value;
-  return mermaidName;
-})();
-
 function saveName() {
   var mermaidName = document.getElementById('mermaid-name').value;
-  document.getElementById('say-hi').innerHTML = "Hello " + myNameIs() + ". You see a boat.";
+  document.getElementById('say-hi').innerHTML = "Hello " + mermaidName + ". You see a boat.";
   document.getElementById("boat-buttons").style.visibility = "visible";
 }
